2272c8941eae57266b36eb84563cbf57aab2ab0a
P-Heckmann <paul.he@posteo.de>
2023-08-29 09:19:16 +0200

    c

--- pages/1_MELTS.py
+++ pages/1_MELTS.py
@@ -93,24 +93,29 @@ def main():
             dt_C=deltat_C,
             P_bar=P,
         )
-        MELTS = MELTS_FC["All"]
 
-        df = st.dataframe(MELTS)
+        st.write(MELTS_FC)
+
+        # MELTS = MELTS_FC["All"]
+
+        # df = st.dataframe(MELTS)
 
         # Add a calculate button
-        @st.cache_data
-        def convert_df(df):
-            # IMPORTANT: Cache the conversion to prevent computation on every rerun
-            return df.to_csv().encode("utf-8")
+        # @st.cache_data
+        # def convert_df(df):
+        #    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        #    return df.to_csv().encode("utf-8")
 
-        csv = convert_df(MELTS)
 
-        st.download_button(
-            label="Download data as CSV",
-            data=csv,
-            file_name="MELTS_output.csv",
-            mime="text/csv",
-        )
+#
+# csv = convert_df(MELTS)
+#
+# st.download_button(
+#    label="Download data as CSV",
+#    data=csv,
+#    file_name="MELTS_output.csv",
+#    mime="text/csv",
+# )
 
 
 # folder_path = '../'

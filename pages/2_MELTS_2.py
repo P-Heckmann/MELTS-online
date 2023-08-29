b7fea0479ede1d6138cfc22026cce380cf87f5b5
P-Heckmann <paul.he@posteo.de>
2023-08-29 09:16:10 +0200

    c

--- pages/2_MELTS_2.py
+++ pages/2_MELTS_2.py
@@ -74,8 +74,11 @@ if calculate_button:
         dt_C=deltat_C,
         P_bar=P,
     )
-    MELTS = MELTS_FC["All"]
-    # df = st.dataframe(MELTS)
+    try:
+        MELTS = MELTS_FC["All"]
+    except KeyError as e:
+        st.write("Error accessing 'All' key in MELTS_FC:", e)
+    df = st.dataframe(MELTS)
 
     # Add a calculate button
     # @st.cache_data
